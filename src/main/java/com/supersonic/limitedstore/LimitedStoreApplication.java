9ecb84b51f6a25f9d387b333d30cee97feccfe9d
BackToTheDev <rnjswnstjd13@gmail.com>
2025-11-25 12:00:57 +0900

    init project

---
+++ src/main/java/com/supersonic/limitedstore/LimitedStoreApplication.java
@@ -0,0 +1,13 @@
+package com.supersonic.limitedstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitedStoreApplication.class, args);
+    }
+
+}
