049159ea9f0705f831ff824ced21c96a6045464f
BackToTheDev <rnjswnstjd13@gmail.com>
2025-12-09 15:27:44 +0900

    feat: ProductControllerTest 구현

--- src/test/java/com/supersonic/limitedstore/product/controller/ProductControllerTest.java
+++ src/test/java/com/supersonic/limitedstore/product/controller/ProductControllerTest.java
@@ -2,12 +2,16 @@ package com.supersonic.limitedstore.product.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.supersonic.limitedstore.common.dto.ApiResponse;
+import com.supersonic.limitedstore.common.exception.CustomException;
+import com.supersonic.limitedstore.common.exception.ErrorCode;
 import com.supersonic.limitedstore.domain.product.presentation.controller.ProductController;
 import com.supersonic.limitedstore.domain.product.presentation.dto.req.ProductRequestDto;
+import com.supersonic.limitedstore.domain.product.presentation.dto.req.ProductUpdateRequestDto;
 import com.supersonic.limitedstore.domain.product.presentation.dto.res.ProductResponseDto;
 import com.supersonic.limitedstore.domain.product.service.ProductService;
 import com.supersonic.limitedstore.security.JwtTokenProvider;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -69,6 +73,44 @@ public class ProductControllerTest {
     }
 
     @Test
+    void 상품_생성_실패_날짜형식_오류() throws Exception {
+        ProductRequestDto dto = ProductRequestDto.builder()
+            .name("테스트 상품")
+            .price(10000)
+            .stock(5)
+            .releaseAt("잘못된 형식")
+            .build();
+
+        when(productService.createProduct(any()))
+            .thenThrow(new CustomException(ErrorCode.INVALID_DATE_FORMAT));
+
+        mockMvc.perform(post("/v1/products")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(dto)))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.status").value(400));
+    }
+
+    @Test
+    void 상품_생성_실패_중복상품명() throws Exception {
+        ProductRequestDto dto = ProductRequestDto.builder()
+            .name("중복 상품")
+            .price(10000)
+            .stock(5)
+            .releaseAt("2025-12-05 12:03:02")
+            .build();
+
+        when(productService.createProduct(any()))
+            .thenThrow(new CustomException(ErrorCode.PRODUCT_DUPLICATED));
+
+        mockMvc.perform(post("/v1/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+            .andExpect(status().isConflict())
+            .andExpect(jsonPath("$.status").value(409));
+    }
+
+    @Test
     void 상품_단건조회_성공() throws Exception {
         // given
         UUID id = UUID.randomUUID();
@@ -87,5 +129,120 @@ public class ProductControllerTest {
         mockMvc.perform(get("/v1/products/{id}", id))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.data.id").value(id.toString()));
+    }
+
+    @Test
+    void 상품_단건조회_실패_NOT_FOUND() throws Exception {
+        UUID id = UUID.randomUUID();
+
+        when(productService.getProductById(id))
+            .thenThrow(new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
+
+        mockMvc.perform(get("/v1/products/{id}", id))
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.status").value(404));
+    }
+
+    @Test
+    void 상품_전체조회_성공() throws Exception {
+        List<ProductResponseDto> list = List.of(
+            ProductResponseDto.builder()
+                .id(UUID.randomUUID())
+                .name("상품1")
+                .price(10000)
+                .stock(5)
+                .releaseAt(LocalDateTime.now())
+                .build(),
+            ProductResponseDto.builder()
+                .id(UUID.randomUUID())
+                .name("상품2")
+                .price(20000)
+                .stock(10)
+                .releaseAt(LocalDateTime.now())
+                .build()
+
+        );
+
+        when(productService.getAllProducts()).thenReturn(ApiResponse.ok(list));
+
+        mockMvc.perform(get("/v1/products"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data").isArray())
+            .andExpect(jsonPath("$.data.length()").value(2));
+    }
+
+    @Test
+    void 상품_업데이트_성공() throws Exception {
+        //given
+
+        UUID id = UUID.randomUUID();
+        ProductUpdateRequestDto dto = ProductUpdateRequestDto.builder()
+            .name("업데이트 상품")
+            .price(20000)
+            .stock(5)
+            .build();
+
+        ProductResponseDto response = ProductResponseDto.builder()
+            .id(id)
+            .name("업데이트 상품")
+            .price(20000)
+            .stock(5)
+            .releaseAt(LocalDateTime.now())
+            .build();
+
+        when(productService.updateProduct(eq(id), any(ProductUpdateRequestDto.class)))
+            .thenReturn(ApiResponse.ok(response));
+
+        //when & then
+        mockMvc.perform(patch("/v1/products/{id}", id)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(dto)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.name").value(dto.getName()))
+            .andExpect(jsonPath("$.data.price").value(dto.getPrice()));
+    }
+
+    @Test
+    void 상품_업데이트_실패_NOT_FOUND() throws Exception {
+        UUID id = UUID.randomUUID();
+
+        ProductUpdateRequestDto dto = ProductUpdateRequestDto.builder()
+            .name("업데이트 상품")
+            .price(20000)
+            .stock(5)
+            .build();
+
+        when(productService.updateProduct(eq(id), any()))
+            .thenThrow(new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
+
+        mockMvc.perform(patch("/v1/products/{id}", id)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(dto)))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void 상품_삭제_성공() throws Exception {
+        // given
+        UUID id = UUID.randomUUID();
+
+        doNothing().when(productService).deleteProduct(id);
+
+        // when & then
+        mockMvc.perform(delete("/v1/products/{id}", id))
+            .andExpect(status().isOk());
+
+        verify(productService, times(1)).deleteProduct(id);
+    }
+
+    @Test
+    void 상품_삭제_실패_NOT_FOUND() throws Exception {
+        UUID id = UUID.randomUUID();
+
+        doThrow(new CustomException(ErrorCode.PRODUCT_NOT_FOUND))
+            .when(productService).deleteProduct(id);
+
+        mockMvc.perform(delete("/v1/products/{id}", id))
+            .andExpect(status().isNotFound());
     }
 }
